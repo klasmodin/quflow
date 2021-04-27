d52af2d4da47b766e8d08ecefef6e36e838484ae
Klas Modin <klas.modin@gmail.com>
2021-04-27 16:27:14 +0200

    Added functionality for creating blobs.

--- quflow/utils.py
+++ quflow/utils.py
@@ -4,9 +4,27 @@ from numba import njit
 
 
 @njit
+def ind2elm(ind):
+    """
+    Convert single index in omega vector to (el, m) indices.
+
+    Parameters
+    ----------
+    ind: int
+
+    Returns
+    -------
+    (el, m): tuple of indices
+    """
+    el = int(np.floor(np.sqrt(ind)))
+    m = ind - el * (el + 1)
+    return el, m
+
+
+@njit
 def elm2ind(el, m):
     """
-    Convert (l,m) spherical harmonics indices to single index
+    Convert (el,m) spherical harmonics indices to single index
     in `omegacomplex` array.
 
     Parameters
@@ -82,3 +100,71 @@ def sphgrid(N):
     theta, phi = pyssht.sample_positions(N, Grid=True)
 
     return theta, phi
+
+
+def so3generators(N):
+    """
+    Return a basis S1, S2, S3 for the representationn of so(3) in u(N).
+
+    Parameters
+    ----------
+    N: int
+
+    Returns
+    -------
+    S1, S2, S3: tuple of ndarray
+    """
+    s = (N-1)/2
+    S3 = 1j*np.diag(np.arange(-s, s+1))
+    S1 = 1j*np.diag(np.sqrt(s*(s+1)-np.arange(-s, s)*np.arange(-s+1, s+1)), 1)/2 + \
+        1j*np.diag(np.sqrt(s*(s+1)-np.arange(-s, s)*np.arange(-s+1, s+1)), -1)/2
+    S2 = np.diag(np.sqrt(s*(s+1)-np.arange(-s, s)*np.arange(-s+1, s+1)), 1)/2 - \
+        np.diag(np.sqrt(s*(s+1)-np.arange(-s, s)*np.arange(-s+1, s+1)), -1)/2
+    return S1, S2, S3
+
+
+def rotate(xi, W):
+    """
+    Apply axis-angle (Rodrigues) rotation to vorticity matrix.
+
+    Parameters
+    ----------
+    xi: ndarray(shape=(3,), dtype=float)
+    W: ndarray(shape=(N,N), dtype=complex)
+
+    Returns
+    -------
+    W_rotated: ndarray(shape=(N,N), dtype=complex)
+    """
+    from scipy.linalg import expm
+    N = W.shape[0]
+    S1, S2, S3 = so3generators(N)
+    R = expm(xi[0]*S1 + xi[1]*S2 + xi[2]*S3)
+    return R@W@R.T.conj()
+
+
+def north_blob(N, sigma = 0):
+    """
+    Return vorticity matrix for blob located at north pole.
+
+    Parameters
+    ----------
+    N: int
+    sigma: float (optional)
+        Gaussian sigma for blob. If 0 (default) then give best
+        approximation to point vortex
+
+    Returns
+    -------
+    W: ndarray(shape=(N, N), dtype=complex)
+    """
+    from .dynamics import solve_heat_flow
+
+    W = np.zeros((N, N), dtype=complex)
+    W[-1, -1] = 1.0j
+
+    if sigma != 0:
+        W = solve_heat_flow(sigma/4., W)
+
+    return W
+
