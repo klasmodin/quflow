23caab388922a2d651055a01b76489d279f82e07
Klas Modin <somnium-math@pm.me>
2023-04-18 23:41:11 +0200

    Separation of dynamics and integrators and global flag for skewherm.

--- quflow/__init__.py
+++ quflow/__init__.py
@@ -3,7 +3,8 @@ from .utils import *
 from .quantization import *
 from .graphics import *
 from .dynamics import *
+from .integrators import *
 from .io import *
 from . import laplacian
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
