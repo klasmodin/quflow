a1a41b9eccdd5ae6b84fba0f6e42fff604058e20
Klas Modin <somnium-math@pm.me>
2023-04-18 23:39:00 +0200

    Separation of dynamics and integrators and global flag for skewherm.

--- quflow/laplacian/__init__.py
+++ quflow/laplacian/__init__.py
@@ -2,3 +2,25 @@ from .direct import *
 from . import direct
 from . import sparse
 from . import tridiagonal
+
+
+def select_skewherm(flag):
+    """
+    Select whether matrices for Laplacian are skew Hermitian.
+
+    Parameters
+    ----------
+    flag: bool
+
+    Returns
+    -------
+    None
+    """
+    if flag:
+        direct.solve_direct_ = solve_direct_skewh_
+        direct.dot_direct_ = dot_direct_skewh_
+    else:
+        direct.solve_direct_ = solve_direct_nonskewh_
+        direct.dot_direct_ = dot_direct_skewh_
+
+    
