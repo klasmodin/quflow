4fef89eb62239f8cf95229876b76f793e6003820
Klas Modin <somnium-math@pm.me>
2023-06-16 15:57:05 +0200

    Update to interface for running on cluster.

--- quflow/templates/vera2_TEMPLATE.sh
+++ quflow/templates/vera2_TEMPLATE.sh
@@ -1,13 +1,14 @@
 #!/bin/env bash
-#SBATCH -A C3SE2022-1-15 -p vera # find your project with the "projinfo" command
+#SBATCH -A C3SE2023-1-2 -p vera # find your project with the "projinfo" command
 #SBATCH -t 4-00:00:00 # maximum simulation time is 4 days
 #SBATCH -J $SIMNAME # name of job
 #SBATCH -n $NO_CORES # number of cores to use
 #SBATCH -N 1 # Use maximum 1 node
 #SBATCH -C SKYLAKE # Use SKYLAKE (slower) or ICELAKE (faster)
 
-module load GCCcore
+# module load GCCcore/11.3.0
 module load FFmpeg
+module load GEOS
 module load Anaconda3
 
 python ./$RUNFILE > $SIMNAME_results.out
